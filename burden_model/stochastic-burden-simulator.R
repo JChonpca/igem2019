c082aae917775b823069272ae2c404edfb9163fe
Ginny Mortensen <48930403+ginnymortensen@users.noreply.github.com>
2020-01-10 01:09:32 -0600

    post edit by JEB

--- burden_model/stochastic-burden-simulator.R
+++ burden_model/stochastic-burden-simulator.R
@@ -1,4 +1,10 @@
 
+
+################################### STOCHASTIC BURDEN SIMULATOR #####################################
+#####  Stochastically plot fraction of engineered cells in a population versus cell doublings  ######
+#####   depending only on the mutation rate and burden                                         ######
+#####################################################################################################
+
 library(ggplot2)
 library(shiny)
 library(tidyr)
@@ -6,78 +12,107 @@ library(dplyr)
 library(deSolve)
 library(adaptivetau)
 
-################################### STOCHASTIC BURDEN SIMULATOR #####################################
-#####   Stochastically plot fraction of engineered cells in a population over generation time  ######
-#####   according to the burden and the mutation rate of the construct                         ######
-#####################################################################################################
+################ Build ODE model #################
 
 pars<- c(
-  b  = 0.2,  # burden
-  u  = 1e-7  # mutation rate
+  b  = 0.4,  # burden
+  u  = 1E-5  # mutation rate
 )
 
-
-burdenmod <- function(t, y, p) {
+burdenode <- function(t, y, p) {
   with(as.list(c(y, p)), {
     dEt <- ((1-b) * Et) - (u * (1-b) * Et)  # rate at which engineered cells (Et) decrease while growing
-    dFt <- Ft + (u * (1-b) * Et)  # rate at which failed cells (Ft) accumulate
+    dFt <- Ft + (u * (1-b) * Et)            # rate at which failed cells (Ft) accumulate
     return(list(c(dEt, dFt)))
   })
 }
 
-yini  <- c(Et = 1, Ft = 0)  # initial values of Et and Ft. Begin with one engineered cell.
-times <- seq(0,200)  # arbitrary number of time steps
+yini  <- c(Et = 1, Ft = 0)       # initial values of Et and Ft. Begin with one engineered cell.
+times <- seq(0,200, 0.1)         # arbitrary number of time steps
 
 ## Run model
-out   <- ode(yini, times, burdenmod, pars)
-  # the model simulates engineered cell growth, mutation, and depletion 
+out   <- ode(yini, times, burdenode, pars)
 
 out <- data.frame(out)
 out$fraction <- (out$Et/(out$Et+out$Ft))  # fraction of engineered cells
-out$generation <- log2(out$Et+out$Ft)  # number of cell doublings
+out$generation <- log2(out$Et+out$Ft)     # number of cell doublings
 summary(out)
 
 ## Plot model
-plot.mod= ggplot(out, aes(x=generation, y=fraction)) +
-  geom_line() +
-# ylim(c(0, 1)) +
-#  xlim(c(0, 200)) +
-  ggtitle("Modeling Burden")+
-  NULL
-plot.mod
+ggplot(out, aes(x=generation, y=fraction)) +
+  geom_line()
 
-######################################################################################
-#one way to use adaptivetau below
-stoburd <- function(x, params, t) {
-  with(params, {
-    return(c(((1-b) * x[1]) - (u * (1-b) * x[1]),  # engineered cells
-             x[2] + (u * (1-b) * x[2])))  # failed cells
-  })
+################# Build stochastic simulator  #################
+
+transition.list = list(
+  c(e = +1),           # trans 1: e cells grow
+  c(f = +1),           # trans 3: f cells grow
+  c(e = -1, f = +1)    # trans 2: e cells mutate into f cells
+)
+
+rates.function <- function(x, params, t) {
+    return(c(
+              (1 - params$b) * x["e"],             # rate of e cells growing
+              x["f"],                              # rate of f cells growing
+              (1 - params$b) * x["e"] * params$u   # rate of e cells turning into f cells
+          )) 
 }
-par=list(b=0.2, u=10^-6, t = 200);
-sbs=ssa.adaptivetau(c(1,0),
-                  matrix(c(0,200, 1,0, 0,-1), nrow=2), stoburd, par, tf=200)
-matplot(r[,"t"], sbs[,c("x1","x2")], type='l', xlab='generations', ylab='Counts')
 
-#####################################################################################
-#more explicit code here
-b=0.1
-u=10^-6
-transitions = list(c(e = +1), # trans 1: e cells grow
-                    c(e = -1, f = +1), # trans 2: e cells mutate into f cells
-                    c(f = +1)) # trans 3: f cells grow
 
-lvRateF <- function(x, params, t) {
-    return(c(params$r * x["e"], # rate of e cells growing
-               params$beta * x["e"],  # rate of e cells turning into f cells
-               params$delta * x["f"]))  # rate of f cells growing
-    }
+## Store all results in a dataframe for easier graphing
+sim.results.df = data.frame()
 
-set.seed(4) # set random number generator seed to be reproducible
-simResults = ssa.adaptivetau(init.values = c(e = 1, f = 0),
-                                transitions, lvRateF,
-                                params = list(r=(1-b), beta=(u*(1-b)), delta=1),
-                                tf=100)
-matplot(simResults[,"time"], simResults[,c("e","f")], type='l',
-             xlab='Time', ylab='Counts (log scale)', log='y')
-legend("bottomleft", legend=c("e", "f"), lty=1:2, col=1:2)
+
+## This loops over different seeds to get replicates
+## It could loop over b and u values as well to populate a table for generating all graphs
+for (this.seed in 1:20) {
+  this.b = 0.4
+  this.u = 1E-5
+  
+  set.seed(this.seed) # set random number generator seed to be reproducible
+  par=list(b=this.b, u=this.u);
+  sim.results = ssa.adaptivetau(
+    init.values = c(e = 1, f = 0),
+    transition.list,
+    rates.function,
+    par,
+    tf=200
+  )
+  
+  #Reformat as a data frame and calculate generations
+  this.sim.results.df =   data.frame (
+    time = sim.results[,c("time")],
+    generation = log2(sim.results[,c("e")] + sim.results[,c("f")]),
+    e = sim.results[,c("e")],
+    f = sim.results[,c("f")],
+    fr.e = sim.results[,c("e")] / (sim.results[,c("e")] + sim.results[,c("f")]),
+    seed = this.seed,
+    b = this.b,
+    u = this.u,
+    method = "stochastic"
+  )
+  sim.results.df = rbind(sim.results.df, this.sim.results.df)
+}
+
+sim.results.df$seed = as.factor(sim.results.df$seed)
+
+ggplot() +
+  geom_line(data = sim.results.df, aes(x=generation, y=fr.e, group=seed), alpha = 0.3)
+
+################### Compare methods ################
+
+ggplot() +
+  geom_line(data = sim.results.df, aes(x=generation, y=fr.e, group=seed), alpha = 0.3)+
+  geom_line(data = out, aes(x=generation, y= fraction), color = "red", alpha = 0.6)+
+  ylab("fraction")
+
+fifty.sim.df=filter(sim.results.df, fr.e < .6 & fr.e > .4)
+fifty.ode.df=filter(out, fraction < .6 & fraction > .4)
+
+ggplot()+
+  geom_point(data = fifty.sim.df, aes(x=generation, y = fr.e))+
+  geom_point(data= fifty.ode.df, aes(x=generation, y= fraction), color = "red")+
+  ylab("fraction")
+
+mean(fifty.sim.df$generation)
+mean(fifty.ode.df$generation)
