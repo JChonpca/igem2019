1cebae658b53408abc5fed39b214c01eb075350c
Ginny Mortensen <48930403+ginnymortensen@users.noreply.github.com>
2020-01-14 15:11:52 -0600

    Not so Shiny aesthetic edit

--- burden_model/burden-ODE-model.R
+++ burden_model/burden-ODE-model.R
@@ -1,29 +1,16 @@
 
+######################################## BURDEN MODEL ###############################################
+###### Fraction of engineered cells in time as predicted by burden (b) and mutation rate (u) ########
+#####################################################################################################
+
 library(ggplot2)
 library(shiny)
 library(tidyr)
 library(dplyr)
 library(deSolve)
-library(adaptivetau)
 
-  ######################################## BURDEN MODEL ###############################################
-  ##### Plot fraction of engineered cells in time as dictated by burden (b) and mutation rate (u) #####
-  #####################################################################################################
-
-#example parameters to test ODE function comment out and 
-#run with burden mod function to test 
-# test_pars<- tibble(
-#   b  = 0.1,  # burden as reduction in growth rate
-#   u  = 1e-5,  # mutation rate
-#   fraction = .5,
-#   this.generation = 200
-# )
-  
-#I need more comments to help me understand what the code is doing -SPL
-#Okay I get it now. But I had to change the structure of vectors / data frames
-#for this different things to get it to work
+## Build ODE
 burdenmod <- function(t, y, p) {
-  ## This function contains the ODE to be passed to the ODE solver
   with(as.list(c(y, p)), {
     dEt <- ((1-b) * Et) - (u * (1-b) * Et)  # rate at which engineered cells decrease while growing
     dFt <- Ft + (u * (1-b) * Et)  # rate at which failed cells accumulate
@@ -32,54 +19,45 @@ burdenmod <- function(t, y, p) {
 }
 
 plot.ode.mod<- function(pars) {
-#this function takes a DATA FRAME of parameters
-#this function reparses that data frame into a new one. Maybe not necessary now
-# pars <- tibble(
-#   b=p$b,
-#   u=p$u,
-#   fraction=p$fraction,
-#   this.generation=p$this.generation
-# )
-print(pars)
-#initial values for Et and Ft
-yini  <- c(Et = 1, Ft = 0)
 
-#build a sequence of times for the solver to use. I removed and extra element in this function
-times <- seq(0, pars$this.generation[1], by = 1)
-
+  print(pars)
+  #initial values for Et and Ft
+  yini  <- c(Et = 1, Ft = 0)
+  
+  #build a sequence of times for the solver to use.
+  times <- seq(0, pars$this.generation[1], by = 1)
+  
 ## Run model
-#Use the ode solver with yini, times, burdenmod function, and parameter df
-out   <- ode(yini, times, burdenmod, pars)
+  #Use the ode solver with initial values, time sequence, function, and parameter dataframe
+  out   <- ode(yini, times, burdenmod, pars)
+  
+  #convert ODE output into data frame for further processing
+  out <- data.frame(out)
+  
+  #calculate fraction of engineered cells 
+  out$fraction <- (out$Et/(out$Et+out$Ft))
+  
+  #calculate generations from number of cell doublings
+  out$generation <- log2(out$Et+out$Ft) 
 
-#convert ODE output into data frame for further processing
-out <- data.frame(out)
-
-#calculate fraction of engineered cells 
-out$fraction <- (out$Et/(out$Et+out$Ft))
-#calculate generations from number of cell doublings
-out$generation <- log2(out$Et+out$Ft) 
-#filter output into new dataframe outout below certain # of generations
-#why is this necessary? -SPL
-outout <- out %>% filter(generation<=pars$this.generation[1])
+# create vector of generations to saturate different volumes
+vol={log2((5*10^9)*(.005*1000))}
+vol2={log2((5*10^9)*(2*1000))}
+vol200={log2((5*10^9)*(200*1000))}
+vol200k={log2((5*10^9)*(200000*1000))}
+volumes<-c(vol, vol2, vol200, vol200k)
 
 ## Plot output of model
-plot.mod <- ggplot(outout, aes(x=generation, y=fraction)) +
+plot.mod <- ggplot(out, aes(x=generation, y=fraction)) +
   geom_line() +
   ylim(c(0, 1)) +
-  xlim(c(0, 150))
+  xlim(c(0, 200)) +
+  geom_vline(xintercept = as.numeric(volumes), color = "#696969")
 plot.mod
+
 }
 
 ## Build Shiny app
-v= 0.005
-v2=2
-v200=200
-v200k=200000
-vol={log2((5*10^9)*(v*1000))}
-vol2={log2((5*10^9)*(v2*1000))}
-vol200={log2((5*10^9)*(v200*1000))}
-vol200k={log2((5*10^9)*(v200k*1000))}
-volumes<-c(vol, vol2, vol200, vol200k)  # generation of saturation for volumes
 
 ## Define UI for application
 ui <- fluidPage(
@@ -89,9 +67,8 @@ ui <- fluidPage(
     radioButtons("u", label = "Mutation Rate", 
                  choiceNames = list("1E-5", "1E-6", "1E-7", "1E-8"), 
                  choiceValues = list(u, 1e-6, 1e-7, 1e-8)),
-    #checkboxGroupInput("volumes", label = "Saturation Volumes:",
-    #                   choices = c("5mL"=vol, "2L"= vol2, "200L"=vol200,"200,000L"=vol200k)),
-    sliderInput("this.generation", label = "Generations", min = 50, max = 200, value = 100)
+    checkboxGroupInput("volumes", label = "Saturation Volumes:",
+                     choices = c("5mL"=vol, "2L"= vol2, "200L"=vol200,"200,000L"=vol200k))
   ),
   mainPanel(
     plotOutput("plotmod")
@@ -103,11 +80,9 @@ server <- function(input, output) {
     pars <- data.frame(
       b = input$b / 100,
       u = as.numeric(input$u),
-      this.generation = input$this.generation
-      #volumes = input$volumes
-      )
+      volumes = input$volumes
+    )
     plot.ode.mod(pars)
-    #ggplot(data = mtcars, aes(x = mpg, y = disp))
   }, 
   height = 600)
 }
@@ -115,12 +90,3 @@ server <- function(input, output) {
 
 ## Run the application 
 shinyApp(ui = ui, server = server)
-
-
-############################# IN PROGRESS - STOCHASTIC SIMULATION ####################################
-# commenting out unused code -SPL
-#parms<-list(b=0.1, u=1e5);
-#stochastic_burden=ssa.adaptivetau(c(1,0),
-#                  matrix(c(,0, -1,1), nrow=2), plot.ode.mod, parms, tf=200)
-#stochastic_burden_plot
-
