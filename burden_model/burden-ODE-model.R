7cc0482efff55820482980e47f2c37fd070c6d3f
spleonard1 <spleonard1@gmail.com>
2020-02-06 21:41:16 -0600

    fixed various issues related to ODE performance

--- burden_model/burden-ODE-model.R
+++ burden_model/burden-ODE-model.R
@@ -2,29 +2,37 @@ library(tidyverse)
 library(shiny)
 library(deSolve)
 
-pars<- c(
-  b = 0.4,   # burden value
-  u = 1e-5   # mutation rate
-)
+#These lines should be deleted, they are global variables but we don't want them as global
+# pars<- c(
+#   b = 0.4,   # burden value
+#   u = 1e-5   # mutation rate
+# )
+#I added some inputs to the 
 
-burdenode <- function(t, y, p) {
-  with(as.list(c(y, p)), {
+burdenode <- function(t, y, pars) {
+  with(as.list(c(y, pars)), {
     dEt <- ((1-b) * Et) - (u * (1-b) * Et)  # rate at which engineered cells (Et) grow/mutate
     dFt <- Ft + (u * (1-b) * Et)            # rate at which failed cells (Ft) accumulate/grow
     return(list(c(dEt, dFt)))
   })
-  
+}
+
+all_the_stuff <- function(burden, mutation_rate) {
   
   yini  <- c(Et = 1, Ft = 0)  # initial values of Et and Ft. Begin with one engineered cell.
   times <- seq(0,200, 0.1)    # time sequence for ODE solver
   
+  pars <- c(b = burden, u = mutation_rate)
   ## Run model
+  
   out   <- ode(yini, times, burdenode, pars)
   out <- data.frame(out)                    # save output into dataframe
+  
   out$fraction <- (out$Et/(out$Et+out$Ft))  # fraction of engineered cells remaining in population
+  
   out$generation <- log2(out$Et+out$Ft)     # number of cell doublings
+  return(out)
 }
-
 
 ## Plot model
 # create vector of generations to saturate different volumes
@@ -35,12 +43,12 @@ vol200k={log2((5*10^9)*(200000*1000))}
 volumes<-c(vol, vol2, vol200, vol200k)
 
 ## Plot output of model
-plot.mod <- ggplot(out, aes(x=generation, y=fraction)) +
-  geom_line() +
-  ylim(c(0, 1)) +
-  xlim(c(0, 200)) +
-  geom_vline(xintercept = as.numeric(volumes), color = "#696969")
-plot.mod
+# plot.mod <- ggplot(out, aes(x=generation, y=fraction)) +
+#   geom_line() +
+#   ylim(c(0, 1)) +
+#   xlim(c(0, 200)) +
+#   geom_vline(xintercept = as.numeric(volumes), color = "#696969")
+# plot.mod
 
 
 
@@ -53,7 +61,7 @@ ui <- fluidPage(
                  choiceNames = list("1E-5", "1E-6", "1E-7", "1E-8"), 
                  choiceValues = list(1e-5, 1e-6, 1e-7, 1e-8)),
     checkboxGroupInput("volumes", label = "Generations to Saturate Volumes:",
-                       choices = c("5mL"=vol, "2L"= vol2, "200L"=vol200,"200,000L"=vol200k))
+                        choices = c("5mL"=vol, "2L"= vol2, "200L"=vol200,"200,000L"=vol200k))
   ),
   mainPanel(
     plotOutput(outputId = "produce")
@@ -62,14 +70,21 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   output$produce <- renderPlot({
-    b <- input$b/100
-    k <- input$k
-    volumes<-input$volumes
-    out.here<- burdenode(t, y, p) 
-    ggplot(data= out.here, aes(x=generation, y = fraction))+geom_line(color = "#ff3232", size = 1.5)+
-      geom_vline(xintercept = as.numeric(volumes), color = "#696969", size = 0.8)+
-      labs(title = "Production Curve", x = "Generations", y = "Fraction of Producing Cells") +
-      NULL
+    b <- as.numeric(input$b)/100
+    k <- as.numeric(input$k)
+    volumes <-input$volumes
+    out.here <- all_the_stuff(burden = b, mutation_rate = k)
+    ggplot(data= out.here, 
+           aes(x=generation, 
+               y = fraction))+
+    geom_line(color = "#ff3232", size = 1.5)+
+    geom_vline(xintercept = as.numeric(volumes), 
+                color = "#696969", 
+                size = 0.8)+
+    labs(title = "Production Curve", 
+         x = "Generations", 
+         y = "Fraction of Producing Cells") +
+    NULL
   }, 
   height = 600)
 }
