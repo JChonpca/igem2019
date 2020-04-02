beea1ebd6b5c603783df7a7bb68260bc82cdcfde
Ginny Mortensen <48930403+ginnymortensen@users.noreply.github.com>
2020-04-02 02:03:53 -0500

    Update burden-ODE-model.R

--- burden_model/burden-ODE-model.R
+++ burden_model/burden-ODE-model.R
@@ -1,8 +1,8 @@
 
-#########################
-# SHINY * IN PROGRESS * #
-#   -GAM                #
-#########################
+##########################
+# BURDEN MODEL SHINY APP #
+#   -GAM                 #
+##########################
 
 
 library(tidyverse)
@@ -20,7 +20,7 @@ burdenode <- function(t, y, pars) {
 
 all_the_stuff <- function(burden, mutation_rate) {
   yini  <- c(Et = 1, Ft = 0)  # initial values of Et and Ft. Begin with one engineered cell.
-  times <- seq(0,200, 0.1)    # time sequence for ODE solver
+  times <- seq(0,100, 0.1)    # time sequence for ODE solver
   pars <- c(b = burden, u = mutation_rate)
   out <- ode(yini, times, burdenode, pars) # run solver
   out <- data.frame(out)                     # save output into dataframe
@@ -48,33 +48,33 @@ rates.function <- function(x, pars, t) {
 sim.results.df = data.frame() # save as a dataframe
 
 all_the_things <- function(burden, mutation_rate){
- for (this.seed in 1:20) {
-   set.seed(this.seed) # set random number generator seed to be reproducible
-   pars = list(b = burden, u = mutation_rate);
-   sim.results = ssa.adaptivetau(
-     init.values = c(e = 1, f = 0),
-     transition.list,
-     rates.function,
-     pars,
-     tf = 200
-   )
-
-  # reformat and calculate generations
-   this.sim.results.df =   data.frame (
-     time = sim.results[,c("time")],
-     generation = log2(sim.results[,c("e")] + sim.results[,c("f")]),
-     e = sim.results[,c("e")],
-     f = sim.results[,c("f")],
-     fr.e = sim.results[,c("e")] / (sim.results[,c("e")] + sim.results[,c("f")]),
-     seed = this.seed,
-     b = burden,
-     u = mutation_rate,
-     method = "stochastic"
-   )
-   sim.results.df = rbind(sim.results.df, this.sim.results.df)
- }
- sim.results.df$seed = as.factor(sim.results.df$seed)
-return(sim.results.df)
+  for (this.seed in 1:20) {
+    set.seed(this.seed) # set random number generator seed to be reproducible
+    pars = list(b = burden, u = mutation_rate);
+    sim.results = ssa.adaptivetau(
+      init.values = c(e = 1, f = 0),
+      transition.list,
+      rates.function,
+      pars,
+      tf = 100
+    )
+    
+    # reformat and calculate generations
+    this.sim.results.df =   data.frame (
+      time = sim.results[,c("time")],
+      generation = log2(sim.results[,c("e")] + sim.results[,c("f")]),
+      e = sim.results[,c("e")],
+      f = sim.results[,c("f")],
+      fr.e = sim.results[,c("e")] / (sim.results[,c("e")] + sim.results[,c("f")]),
+      seed = this.seed,
+      b = burden,
+      u = mutation_rate,
+      method = "stochastic"
+    )
+    sim.results.df = rbind(sim.results.df, this.sim.results.df)
+  }
+  sim.results.df$seed = as.factor(sim.results.df$seed)
+  return(sim.results.df)
 }
 
 ########################################################################################################
@@ -95,7 +95,7 @@ ui <- fluidPage(
     radioButtons("u", label = "Escape Rate", 
                  choiceNames = list("1E-5", "1E-6", "1E-7", "1E-8"), 
                  choiceValues = list(1e-5, 1e-6, 1e-7, 1e-8)),
-    checkboxGroupInput("volumes", label = "Generations to Saturate Volumes:",
+    radioButtons("volumes", label = "Generations to Saturate Volumes:",
                        choices = c("5mL"=vol, "2L"= vol2, "200L"=vol200,"200,000L"=vol200k))
   ),
   mainPanel(
@@ -117,11 +117,11 @@ server <- function(input, output) {
       geom_line(data = out.there, aes(x = generation, y = fr.e, group = seed)) +
       geom_line()+
       geom_vline(xintercept = as.numeric(volumes), 
-                 color = "#696969", 
+                 color = "blue", 
                  size = 0.8) +
       labs(title = "Production Curve", 
-           x = "Generations", 
-           y = "Fraction of Producing Cells") +
+           x = "Cell Doublings", 
+           y = "Fraction of Engineered Cells") +
       NULL
   }, 
   height = 600)
